1bf486bc9d0fbc5052830120b49e42f9b80c052d
Gaurav-Mukherjee <gauravmukherjee69@gmail.com>
2023-11-08 23:10:42 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#FizzBuzz Challenge
+
+for num in range(1,101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("fizzbuzz")
+    elif num % 5 == 0:
+        print("buzz")
+    elif num % 3 == 0:
+        print("fizz")
+    else:
+        print(num)
