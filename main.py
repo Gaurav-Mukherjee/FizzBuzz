37755411de9426506d36485fd4dca94fe6aaf71a
Gaurav-Mukherjee <gauravmukherjee69@gmail.com>
2023-11-08 23:36:03 +0530

    1.1 User Input

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #FizzBuzz Challenge
 
-for num in range(1,101):
-    if num % 3 == 0 and num % 5 == 0:
-        print("fizzbuzz")
-    elif num % 5 == 0:
-        print("buzz")
-    elif num % 3 == 0:
-        print("fizz")
-    else:
-        print(num)
+choice = int(input("Enter maximum range: "))
+choice = choice + 1
+
+def FizzBuzz(choice):
+    for num in range(1,choice):
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuzz")
+        elif num % 5 == 0:
+            print("buzz")
+        elif num % 3 == 0:
+            print("fizz")
+        else:
+            print(num)
+
+FizzBuzz(choice)
